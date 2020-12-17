0c150e72e897b9b3b1e156de4ed8d95bd2ed8a44
okorkov <alex.okorkov@gmail.com>
2020-12-16 19:40:30 -0800

    commit tree 403eaa5e646e02715bc40c5cc8ff96667c83c46b

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,53 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect '/articles'
   end
+
+  get '/articles' do 
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do 
+    erb :new
+  end
+
+  get '/articles/:id' do 
+    @id = params[:id]
+    @article = Article.find(@id)
+    erb :show
+  end
+
+  delete '/articles/:id' do 
+    Article.find(params[:id]).destroy
+    redirect '/articles'
+  end
+
+  post '/articles' do 
+    Article.create(title: params[:title], content: params[:content])
+    redirect "/articles/#{Article.last.id}"
+  end
+
+  get '/articles/:id/edit' do
+    @id = params[:id]
+    @article = Article.find(@id)
+    erb :edit
+  end
+
+  patch '/articles/:id' do 
+    @patched = Article.find(params[:id])
+    @patched.title = params[:title]
+    @patched.content = params[:content]
+    @patched.save
+    redirect "/articles/#{@patched.id}"
+  end
+
+
 end
+
+{"title"=>"is Alex Okarkau as famous as he seems to be?", 
+"content"=>"Well the thing is that Alex Okarkau is just awesome and you kinda gotta 
+deal with it and there is nothing you can do cause you cannot possibly
+   be more awesome than Alex Okarkau so beat it loosers.",
+  "_method"=>"patch", "id"=>"9"}
--- config.ru
+++ config.ru
@@ -5,9 +5,10 @@ begin
   fi_check_migration
 
   use Rack::MethodOverride
-
-  run ApplicationController
+  run ApplicationController 
+  
 rescue ActiveRecord::PendingMigrationError => err
   STDERR.puts err
   exit 1
 end
+
